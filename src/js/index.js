ee16ed115fdbdefa2a5b4250dae5c90bde8c7694
Joanna Szefer <jo.szefer@gmail.com>
2021-02-03 17:04:21 +0100

    Task 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
 /* Here goes your JS code */
+document
+  .getElementById("show-popup-form")
+  .addEventListener("click", setVisibility);
+document
+  .getElementById("close-popup-btn")
+  .addEventListener("click", setVisibility);
+
+function setVisibility(e) {
+  e.preventDefault();
+  const loginForm = document.getElementById("login-form");
+  loginForm.classList.toggle("invisible");
+  const clickMeBtn = document.getElementById("show-popup-form");
+  clickMeBtn.classList.toggle("invisible");
+}
