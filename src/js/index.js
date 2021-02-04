431076d9c238f9c6968c8fa9bc8b1e7638855b75
Joanna Szefer <jo.szefer@gmail.com>
2021-02-04 13:04:00 +0100

    Task 2

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,29 @@ function setVisibility(e) {
   const clickMeBtn = document.getElementById("show-popup-form");
   clickMeBtn.classList.toggle("invisible");
 }
+const submitBtn = document.getElementById("submitBtn");
+
+const formValidation = (e) => {
+  e.preventDefault();
+
+  let isCorrect = false;
+  const emailAddress = document.getElementById("email");
+  const password = document.getElementById("password");
+  const checkbox = document.getElementById("checkbox");
+  const regex = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}/;
+
+  if (emailAddress.value === "") {
+    alert("Please enter your e-mail address!");
+    emailAddress.focus();
+  } else if (password.value === "") {
+    alert("Please enter your password!");
+    password.focus();
+  } else if (!regex.test(emailAddress.value)) {
+    alert("Invalid email format");
+  } else if (!checkbox.checked) {
+    alert("Please accept terms & conditions");
+  } else isCorrect = true;
+
+  return isCorrect;
+};
+submitBtn.addEventListener("click", formValidation);
